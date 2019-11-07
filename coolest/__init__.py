69774478405de6e5e78a2fe830289f5c8f3606ba
Yeonchan Ahn <acha21@europa.snu.ac.kr>
2019-11-07 14:15:06 +0900

    created basic structure files and folder

---
+++ coolest/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2019 Yeonchan Ahn (acha21@europa.snu.ac.kr)  
+# This software is released under the MIT License.
+# http://opensource.org/licenses/mit-license.php
+
+
+def joke():
+    return (u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
+            u'Beiherhund das Oder die Flipperwaldt gersput.')
---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2019 Yeonchan Ahn (acha21@europa.snu.ac.kr)  
+# This software is released under the MIT License.
+# http://opensource.org/licenses/mit-license.php
+
+from setuptools import setup
+
+setup(name='coolest',
+      version='0.1',
+      description='The coolest joke in the world',
+      url='http://github.com/acha21/coolest',
+      author='Yeonchan Ahn',
+      author_email='ahnyeonchan@gmail.com',
+      license='MIT',
+      packages=['coolest'],
+      zip_safe=False)
